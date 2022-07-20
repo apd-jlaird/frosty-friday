b6f029a1d50f1fc6ba37d0d2731d22dd75fa91f6
apd-jlaird <jamie.laird@aimpointdigital.com>
2022-07-20 11:26:31 +0100

    initial commit

---
+++ week_1/s3-to-snowflake.sql
@@ -0,0 +1,18 @@
+-- set context
+use role 
+use warehouse dev_wh;
+use database 
+
+-- create database and schema
+create database frosty_friday;
+create schema week_1;
+
+-- create stage
+
+-- create file format
+
+-- create table
+
+-- copy into
+
+-- cross-check row count between stage and table
