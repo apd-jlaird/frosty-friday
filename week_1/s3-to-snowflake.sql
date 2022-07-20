ea2bcfae0f318000b0f87dfce19e19e2807b3aef
apd-jlaird <jamie.laird@aimpointdigital.com>
2022-07-20 12:30:56 +0100

    added stage

--- week_1/s3-to-snowflake.sql
+++ week_1/s3-to-snowflake.sql
@@ -1,17 +1,31 @@
+------------------------------------------------------------
+-- FROSTY FRIDAY - WEEK 1
+------------------------------------------------------------
 -- set context
-use role 
+use role sandbox_dba;
 use warehouse dev_wh;
-use database 
+use database sandbox;
 
--- create database and schema
-create database frosty_friday;
-create schema week_1;
+-- create schema
+create schema ff_week_1;
 
--- create stage
+-- create stage and list files
+create stage ff_week_1_stage url='s3://frostyfridaychallenges/challenge_1/';
+
+-- list files in stage
+list  @ff_week_1_stage;
 
 -- create file format
+create or replace file format ff_week_1_csv
+    type = 'csv';
+
+-- preview 10 columns of each file
+select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10 from @ff_week_1_csv_stage (file_format => 'ff_week_1_csv_file_format', pattern=>'challenge_1/1.csv');
+select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10 from @ff_week_1_csv_stage (file_format => 'ff_week_1_csv_file_format', pattern=>'challenge_1/2.csv');
+select $1,$2,$3,$4,$5,$6,$7,$8,$9,$10 from @ff_week_1_csv_stage (file_format => 'ff_week_1_csv_file_format', pattern=>'challenge_1/3.csv');
 
 -- create table
+
 
 -- copy into
 
