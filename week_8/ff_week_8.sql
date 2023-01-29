a3f9a21959992da044797a8d96149875cb0e1a5b
apd-jlaird <jamie.laird@aimpointdigital.com>
2023-01-29 16:46:21 +0000

    added script

---
+++ week_8/ff_week_8.sql
@@ -0,0 +1,7 @@
+------------------------------------------------------------
+-- Frosty Friday Week 7
+-- https://frostyfriday.org/2022/08/05/week-8-basic/
+------------------------------------------------------------
+use role securityadmin;
+use database jamielaird;
+use schema frosty_friday;
