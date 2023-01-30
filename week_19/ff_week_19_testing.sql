7032391ad958ac4685174fcb74a1363b31176e84
apd-jlaird <jamie.laird@aimpointdigital.com>
2023-01-30 17:57:48 +0000

    added testing script

---
+++ week_19/ff_week_19_testing.sql
@@ -0,0 +1,32 @@
+------------------------------------------------------------
+-- Frosty Friday Week 19 - Testing Code
+------------------------------------------------------------
+use role jamielaird;
+use database jamielaird;
+use schema frosty_friday;
+
+create table ff_week_19_testing (
+id INT,
+start_date DATE,
+end_date DATE
+);
+insert into ff_week_19_testing (id, start_date, end_date) values (1, '11/11/2020', '9/3/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (2, '12/8/2020', '1/19/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (3, '12/24/2020', '1/15/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (4, '12/5/2020', '3/3/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (5, '12/24/2020', '6/20/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (6, '12/24/2020', '5/19/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (7, '12/31/2020', '5/6/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (8, '12/4/2020', '9/16/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (9, '11/27/2020', '4/14/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (10, '11/20/2020', '1/18/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (11, '12/1/2020', '3/31/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (12, '11/30/2020', '7/5/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (13, '11/28/2020', '6/19/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (14, '12/21/2020', '9/7/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (15, '12/13/2020', '8/15/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (16, '11/4/2020', '3/22/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (17, '12/24/2020', '8/29/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (18, '11/29/2020', '10/13/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (19, '12/10/2020', '7/31/2022');
+insert into ff_week_19_testing (id, start_date, end_date) values (20, '11/1/2020', '10/23/2021');
