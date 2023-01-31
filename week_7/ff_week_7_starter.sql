f46f8c8d760392fbd06ecf301bc1372dd87a6027
apd-jlaird <jamie.laird@aimpointdigital.com>
2023-01-31 11:14:51 +0000

    code refactoring

--- week_7/ff_week_7_starter.sql
+++ week_7/ff_week_7_starter.sql
@@ -2,7 +2,6 @@
 -- Frosty Friday Week 7 - Starter Code
 ------------------------------------------------------------
 use role jamielaird;
-use warehouse jamielaird;
 use database jamielaird;
 use schema frosty_friday;
 
