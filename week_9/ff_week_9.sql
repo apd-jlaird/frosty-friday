3e93ed1e05b5f86ee1e354b4a6d8c967109c10bc
apd-jlaird <jamie.laird@aimpointdigital.com>
2023-02-13 09:41:59 +0000

    minor edits

---
+++ week_9/ff_week_9.sql
@@ -0,0 +1,8 @@
+------------------------------------------------------------
+-- Frosty Friday Week 9
+-- https://frostyfriday.org/2022/08/12/week-9-intermediate/
+------------------------------------------------------------
+use role jamielaird;
+use database jamielaird;
+use schema frosty_friday;
+
