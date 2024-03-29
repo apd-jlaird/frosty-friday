43e1717afda3a60e53415a7f991fd8909a0e75cb
Jamie Laird <jamie.laird@aimpointdigital.com>
2024-03-29 14:24:49 +0000

    added solution

--- week_87/ff_week_87.sql
+++ week_87/ff_week_87.sql
@@ -1,23 +1,37 @@
 ------------------------------------------------------------
--- Frosty Friday Week X
--- URL
+-- Frosty Friday Week 87
+-- https://frostyfriday.org/blog/2024/03/29/week-87-basic/
 ------------------------------------------------------------
-use role jamielaird;
-use database jamielaird;
-use schema frosty_friday;
+use role JAMIELAIRD;
+use database JAMIELAIRD;
+use schema FROSTY_FRIDAY;
 
 ------------------------------------------------------------
 -- Startup Code
 ------------------------------------------------------------
+create or replace table WEEK_87 as
+select 
+  'Happy Easter' as GREETING,
+  array_construct('DE', 'FR', 'IT', 'ES', 'PL', 'RO', 'JA', 'KO', 'PT') as LANGUAGE_CODES
+;
 
 ------------------------------------------------------------
 -- Solution
 ------------------------------------------------------------
 
+select
+    GREETING,
+    VALUE as LANGUAGE_CODE,
+    SNOWFLAKE.CORTEX.TRANSLATE(GREETING, 'EN', LANGUAGE_CODE) as TRANSLATED_GREETING
+from WEEK_87,
+    lateral flatten(input => LANGUAGE_CODES);
+
 ------------------------------------------------------------
 -- Cleanup
 ------------------------------------------------------------
+drop table if exists WEEK_87;
 
 ------------------------------------------------------------
 -- References
 ------------------------------------------------------------
+-- https://docs.snowflake.com/en/sql-reference/functions/translate-snowflake-cortex
